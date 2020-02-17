2e53f7f678b91c7af3a3491013a25213c46aada2
David Eisenbaum <DavidJEisenbaum@Gmail.com>
2020-02-17 13:30:52 -0800

    Connected textfield (test)

--- app/src/main/java/com/example/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/inventorymanager/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.inventorymanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView helloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloWorld = findViewById(R.id.helloWorldID);
+
+        helloWorld.setText("ANY TRUERS");
     }
 }
