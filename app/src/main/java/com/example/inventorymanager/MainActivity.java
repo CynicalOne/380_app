cec07502025515097bf6684ad02e021403202808
David Eisenbaum <DavidJEisenbaum@Gmail.com>
2020-02-19 18:52:55 -0800

    test

--- app/src/main/java/com/example/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/inventorymanager/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //testign master branch connection to github February 19, 2020 6:52 PM
     }
 }
