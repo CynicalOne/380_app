8847d802a45fe2b29afa61de5af0c1dd1503fa2f
CynicalOne <cynicalonemusic@gmail.com>
2020-03-02 19:52:30 -0800

    added editmode and listedit interfaces

---
+++ app/src/main/java/com/example/inventorymanager/EditMode.java
@@ -0,0 +1,5 @@
+package com.example.inventorymanager;
+
+public interface EditMode {
+    void editName(String name);
+}
---
+++ app/src/main/java/com/example/inventorymanager/ListEdit.java
@@ -0,0 +1,5 @@
+package com.example.inventorymanager;
+
+public interface ListEdit {
+    void showDeleteButton();
+}
