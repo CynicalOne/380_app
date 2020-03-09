4153ac3bc0a7183da4a1c7f08280958ee8892306
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-03-09 14:06:01 -0700

    Create ProfileView class skeleton.

---
+++ app/src/main/java/com/example/inventorymanager/ProfileView.java
@@ -0,0 +1,39 @@
+public class ProfileView implements ListEdit<Location>
+{
+    public Profile profile;
+    public CardView LocationsDisplayStyle;
+    public TextField changeNameTextField;
+    public Button changeProfilePic;
+    public Button makeBusiness;
+    public Button makePersonal;
+    public Button addLocationButton;
+    public Button editButton;
+    public boolean inEditMode;
+
+    // Default constructor
+    public ProfileView()
+
+    public void goToLocationView(Location) {
+
+    }
+
+    public void deleteListElement(Location locationToDelete){
+
+    }
+
+    public void deleteEntireList() {
+
+    }
+
+    public void addListElement(Location locationToAdd) {
+
+    }
+
+    public void clickAddButton() {
+
+    }
+
+    public void clickEditButton() {
+        
+    }
+}
