8d16768103584f2de36ecea096e4f62bee398de1
David Eisenbaum <DavidJEisenbaum@Gmail.com>
2020-03-11 13:35:58 -0700

    fixed generic braces error

--- app/src/main/java/com/example/inventorymanager/ListEdit.java
+++ app/src/main/java/com/example/inventorymanager/ListEdit.java
@@ -3,7 +3,7 @@ package com.example.inventorymanager;
 public interface ListEdit<T>{
     void deleteListElement(int index);
     void deleteEntireList();
-    void addListElement(<E> e);
+    void addListElement(T t);
 
     void clickAddButton();
     void clickEditButton();
