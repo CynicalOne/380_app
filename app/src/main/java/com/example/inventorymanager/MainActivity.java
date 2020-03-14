d1bb0b3cc9fe46ebf2d5d3d7bb0694e8150f29a9
David Eisenbaum <DavidJEisenbaum@Gmail.com>
2020-03-14 11:21:04 -0700

    working on recycler view for homeview

--- app/src/main/java/com/example/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/inventorymanager/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.inventorymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView helloWorld;
+    private RecyclerView recyclerView;
+    //private RecyclerViewAdapter adapter;
+    private ArrayList<Location> locationArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect recyler view
+        recyclerView = findViewById(R.id.recyclerview);
 
 
     }
