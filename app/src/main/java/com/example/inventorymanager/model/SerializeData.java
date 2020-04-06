e3b0f286f05ffaec7d5a8fc2e20eb761ea2c2c01
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-04-06 01:51:03 -0700

    Testing still needed to be done

--- app/src/main/java/com/example/inventorymanager/model/SerializeData.java
+++ app/src/main/java/com/example/inventorymanager/model/SerializeData.java
@@ -13,26 +13,47 @@ public class SerializeData implements Serializable
 {
     Gson gson = new Gson();
     Type profileListType = new TypeToken<ArrayList<Profile>>(){}.getType();
-    Type locationListType = new TypeToken<ArrayList<Location>>(){}.getType();
-    Type itemListType = new TypeToken<ArrayList<Item>>(){}.getType();
+    //Type locationListType = new TypeToken<ArrayList<Location>>(){}.getType();
+    //Type itemListType = new TypeToken<ArrayList<Item>>(){}.getType();
 
-    public void serializeProfiles(ArrayList<Profile> profiles) {
+    public void serializeProfiles(ArrayList<Profile> profiles, Context context) {
         try {
             String jsonStr = gson.toJson(profiles);
-            FileWriter writer = new FileWriter("profiles.json");
-            writer.write(jsonStr);
-            writer.close();
-        } 
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("profiles.json", Context.MODE_PRIVATE));
+            outputStreamWriter.write(jsonStr);
+            outputStreamWriter.close();
+        }
         catch (IOException e) {
-            System.out.println("exception " + e.getMessage());
-            e.printStackTrace();
+            Log.e("Exception", "File write failed: " + e.toString());
         }
     }
 
     public ArrayList<Profile> deserializeProfiles() {
         String file = "profiles.json";
-        Path path = Paths.get(file);
-        String jsonStr = Files.readString(path);
+        String jsonStr = "";
+        try {
+            InputStream inputStream = context.openFileInput(file);
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String receiveString = "";
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((receiveString = bufferedReader.readLine()) != null) {
+                stringBuilder.append("\n").append(receiveString);
+            }
+
+            inputStream.close();
+            jsonStr = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } 
+        catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
         ArrayList<Profile> profiles = gson.fromJson(jsonStr, profileListType);
         return profiles;
     }
