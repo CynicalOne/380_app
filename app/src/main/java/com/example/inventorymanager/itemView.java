da719a6561ca99ba5bf5f968603c1f3a46cef9e7
josh-tech <52065677+josh-tech@users.noreply.github.com>
2020-04-29 09:11:48 -0700

    itemView

---
+++ app/src/main/java/com/example/inventorymanager/itemView.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class itemView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_view);
+    }
+}
