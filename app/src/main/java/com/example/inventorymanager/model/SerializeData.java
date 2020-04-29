025d1bde7c7a0ca1fe1e34c39edfda7c219e8521
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-04-29 15:25:25 -0700

    Items to items

--- app/src/main/java/com/example/inventorymanager/model/SerializeData.java
+++ app/src/main/java/com/example/inventorymanager/model/SerializeData.java
@@ -24,7 +24,7 @@ public class SerializeData
         // Converts the location (without the Items ArrayList) into JSON format
         locationJSONStr = gson.toJson(location);
         // Converts the Location's Items ArrayList into JSON format
-        itemsJSONstr = gson.toJson(location.Items);
+        itemsJSONstr = gson.toJson(location.items);
         // Create Pair object
         Pair PairToSerialize = new Pair(locationJSONStr, itemsJSONstr);
         // Return one string that can be stored in SQLite
@@ -34,7 +34,7 @@ public class SerializeData
     public static Location deserializeLocationAndItems(String pairJSONStr) {
         Pair pair = gson.fromJson(pairJSONStr, PairType);
         Location location = gson.fromJson(pair.serializedLocation, LocationType);
-        location.Items = gson.fromJson(pair.serializedItems, itemListType);
+        location.items = gson.fromJson(pair.serializedItems, itemListType);
         return location;
     }
 }
