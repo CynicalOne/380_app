9146c1d0140ff7fb929edddf2fe1cb67c7640560
FeTree <davidjeisenbaum@gmail.com>
2020-05-02 16:06:39 -0700

    view working

---
+++ app/src/main/java/com/example/inventorymanager/ViewBusinessItemsActivity.java
@@ -0,0 +1,15 @@
+package com.example.inventorymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewBusinessItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_business_items);
+
+    }
+}
