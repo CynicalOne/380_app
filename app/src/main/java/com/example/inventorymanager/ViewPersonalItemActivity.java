23b7b39b6323e03495fe8f6c0761eb919320c577
FeTree <davidjeisenbaum@gmail.com>
2020-05-12 20:23:05 -0700

    Everything works... kinda

--- app/src/main/java/com/example/inventorymanager/ViewPersonalItemActivity.java
+++ app/src/main/java/com/example/inventorymanager/ViewPersonalItemActivity.java
@@ -3,12 +3,16 @@ package com.example.inventorymanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.inventorymanager.Persistence.DatabaseHandler_Location;
 import com.example.inventorymanager.model.Location;
@@ -36,6 +40,9 @@ public class ViewPersonalItemActivity extends AppCompatActivity {
 
     Button savePersonalItemButton;
 
+    // Image request code
+    private static int RESULT_LOAD_IMAGE = 1;
+
     DatabaseHandler_Location db = new DatabaseHandler_Location(this);
 
     @Override
@@ -57,6 +64,7 @@ public class ViewPersonalItemActivity extends AppCompatActivity {
         priceEdit = findViewById(R.id.priceLocEditText);
         dateOfPurchase = findViewById(R.id.dateOfPurchaseLoc);
         savePersonalItemButton = findViewById(R.id.saveButtonLoc);
+        setImageButton = findViewById(R.id.setImageLocation);
 
         nameOfLocationItem.setText(location.getLocationName());
         descriptionOfLocationItem.setText(location.getAddress());
@@ -83,6 +91,16 @@ public class ViewPersonalItemActivity extends AppCompatActivity {
                 updatePersonalItem(location);
             }
         });
+
+        //Pick image
+        setImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
     }
 
     private void updatePersonalItem(Location location) {
@@ -103,5 +121,26 @@ public class ViewPersonalItemActivity extends AppCompatActivity {
         //refresh activity
         startActivity(getIntent());
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            try {
+                Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+                String path = selectedImage.getPath();
+
+                ImageView imageView = (ImageView) findViewById(R.id.locationItemPic);
+                imageView.setImageBitmap(bm);
+                //Toast.makeText(this, path, Toast.LENGTH_LONG).show();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
